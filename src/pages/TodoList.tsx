755fb1917b8a3c15f066db4f1daf08946e0eb9af
Gabriel Rodrigues <gabriel-rodriguesbs@hotmail.com>
2023-11-23 18:19:51 -0300

    feat: Adds TodoList integration with back end api

--- src/pages/TodoList.tsx
+++ src/pages/TodoList.tsx
@@ -1,11 +1,35 @@
 import { Link } from 'react-router-dom';
 import '../styles/styles.css';
+import { useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFile, faTrash } from '@fortawesome/free-solid-svg-icons';
+import TodoListController from '../controllers/TodoListController';
 
 const TodoList = () => {
+  const {
+    fetchData,
+    todo,
+    items,
+    setNewItem,
+    addItem,
+    removeItem,
+    removeAll,
+    fetchItems,
+  } = TodoListController();
+
+  useEffect(() => {
+    fetchData();
+  }, [typeof window !== undefined]);
+  useEffect(() => {
+    fetchItems();
+  }, [todo]);
+  useEffect(() => {
+    console.log(items);
+  }, [items]);
   return (
     <div className='box'>
       <h1 className='titulo'>
-        <i className='fa-regular fa-file'></i>
+        <FontAwesomeIcon icon={faFile} />
         Todo List
       </h1>
       <div className='header'>
@@ -13,17 +37,43 @@ const TodoList = () => {
           type='text'
           id='todoInput'
           placeholder='Adicione aqui suas tarefas'
+          onChange={(e) => setNewItem(e.currentTarget.value)}
           maxLength={50}
         />
-        <button id='btnAdd'> + </button>
+        <button onClick={addItem} id='btnAdd'>
+          {' '}
+          +{' '}
+        </button>
       </div>
-      <div id='content'>{/* CONTENT */}</div>
-      <div className='footer'>
-        <p>
-          Você tem <span id='count'>4</span> Tarefas
-        </p>
-        <button id='btnClearAll'> Limpar </button>
-      </div>
+      {items?.length > 0 ? (
+        <>
+          <div id='content'>
+            {items.map(({ id, description }) => (
+              <div className='todo'>
+                <p>{description}</p>
+                <button
+                  className='btnClear'
+                  data-target={id}
+                  onClick={() => removeItem(id)}
+                >
+                  <FontAwesomeIcon icon={faTrash} />
+                </button>
+              </div>
+            ))}
+          </div>
+          <div className='footer'>
+            <>
+              <p>
+                Você tem <span id='count'>{items?.length}</span> Tarefas
+              </p>
+              <button id='btnClearAll' onClick={removeAll}>
+                {' '}
+                Limpar{' '}
+              </button>
+            </>
+          </div>
+        </>
+      ) : null}
       <div className='ava'>
         <Link className='voltar' to='/'>
           Voltar
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,27 @@
 import { Link } from 'react-router-dom';
 import '../styles/style.css';
 import { useNavigate } from 'react-router-dom';
-
+import axios from 'axios';
+import cookie from 'cookiejs';
 const Index = () => {
+  const handleLogin = async () => {
+    const { data } = await axios.post(
+      'http://localhost:3000/auth/login',
+      {
+        email: 'dev@todo.com.br',
+        password: 'my-secret',
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+    if (data) {
+      cookie.set('token', data.access_token);
+      navigate('/todo');
+    }
+  };
   const navigate = useNavigate();
   return (
     <>
@@ -22,7 +41,8 @@ const Index = () => {
         <input
           className='nome nome3'
           onClick={(e) => {
-            e.preventDefault(), navigate('/todo');
+            e.preventDefault();
+            handleLogin();
           }}
         />
       </form>
