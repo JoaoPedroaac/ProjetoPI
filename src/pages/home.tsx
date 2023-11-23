0ddc79afd3b71052b3cef5620dc1263111705c57
Gabriel Rodrigues <gabriel-rodriguesbs@hotmail.com>
2023-11-23 18:36:00 -0300

    fix: Adjusts style

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,15 +3,15 @@ import '../styles/pag3.css';
 const Home = (): JSX.Element => {
   return (
     <div className='container'>
-      <div className='Cabeçalho'>
+      <div className='cabecalho'>
         <img src='/assets/img/WithoutBackground.png' alt='Logo' />
       </div>
 
-      <div className='Agradecimento'>
-        <div className='Notificação'>
+      <div className='agradecimento'>
+        <div className='notificacao'>
           <h2>Pesquisa de Funcionalidade!</h2>
         </div>
-        <div className='Notificação2'>
+        <div className='notificacao2'>
           <h3>A sua opinião é fundamental para o nosso aprimoramento.</h3>
           <Link className='but' to={'/avaliacao'}>
             Iniciar
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,18 @@ import '../styles/style.css';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import cookie from 'cookiejs';
+import { useState } from 'react';
 const Index = () => {
+  const [email, setEmail] = useState<string>();
+  const [password, setPassword] = useState<string>();
+  const [showPassword, setShowPassword] = useState<boolean>(false);
   const handleLogin = async () => {
+    if (!email || !password) return;
     const { data } = await axios.post(
       'http://localhost:3000/auth/login',
       {
-        email: 'dev@todo.com.br',
-        password: 'my-secret',
+        email,
+        password,
       },
       {
         headers: {
@@ -26,25 +31,48 @@ const Index = () => {
   return (
     <>
       <div className='cabecalho'>
-        <a>Antler Moose</a>
+        <a style={{ fontSize: ' 32px' }}>Antler Moose</a>
       </div>
       <div className='redes'>
         <img className='Logo' src='/assets/img/WithoutBackground.png' alt='' />
       </div>
 
       <form className='ipt'>
-        <input className='nome' type='text' placeholder='E-mail' />
-        <input className='nome nome2' type='password' placeholder='Senha' />
+        <input
+          className='nome'
+          type='text'
+          placeholder='E-mail'
+          onChange={(e) => setEmail(e.currentTarget.value)}
+        />
+        <input
+          className='nome nome2'
+          type={showPassword ? 'text' : 'password'}
+          placeholder='Senha'
+          onChange={(e) => setPassword(e.currentTarget.value)}
+        />
+        <fieldset>
+          <input
+            type='checkbox'
+            name='showPassword'
+            id='show-password'
+            onClick={() => setShowPassword(!showPassword)}
+            checked={showPassword}
+          />
+          Mostrar Senha
+        </fieldset>
         <Link className='ResetS' to={'/Todo'}>
           Esqueceu sua senha?
         </Link>
-        <input
+        <button
+          style={{ textAlign: 'center' }}
           className='nome nome3'
           onClick={(e) => {
             e.preventDefault();
             handleLogin();
           }}
-        />
+        >
+          Login
+        </button>
       </form>
     </>
   );
